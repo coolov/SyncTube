70ab19c81b0c522a7e72dc508e91e549ca6f02ae
Reza Nikoopour <rnikoopour@gmail.com>
2015-12-09 22:08:52 -0800

    Giving credit where credit is due

--- reactModules/videoPlayer/ytApi.js
+++ reactModules/videoPlayer/ytApi.js
@@ -1,3 +1,8 @@
+/*
+This was written by Github user helmutkian (https://github.com/helmutkian)
+
+https://github.com/helmutkian/browserify-youtubeIframeApi
+*/
 var Q = require('q');
 
 var apiDeferred = Q.defer();
